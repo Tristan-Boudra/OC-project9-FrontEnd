5efe91180b8abdc86feb5b51e7d1ce0e75ba9bab
Tristan-Boudra <boudratristan@gmail.com>
2023-12-25 09:54:01 +0100

    fix(login): Add old code for presentation

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -71,6 +71,27 @@ export default class Login {
         })
     }
   }
+  
+  // handleSubmitAdmin = e => {
+  //   e.preventDefault()
+  //   const user = {
+  //     type: "Admin",
+  //     email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
+  //     password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+  //     status: "connected"
+  //   }
+  //   this.localStorage.setItem("user", JSON.stringify(user))
+  //   this.login(user)
+  //     .catch(
+  //       (err) => this.createUser(user)
+  //     )
+  //     .then(() => {
+  //       this.onNavigate(ROUTES_PATH['Dashboard'])
+  //       this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
+  //       PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
+  //       document.body.style.backgroundColor="#fff"
+  //     })
+  // }
 
   // not need to cover this function by tests
   login = (user) => {
